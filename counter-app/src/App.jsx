ad6effb5f2076ef0c2d61768c7aca6276488ad1c
Manas Kumar Pradhan <pkmanas22@gmail.com>
2024-09-23 23:14:37 +0530

    L-5 Assignment

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 
+// L-5 Assignment:-  Write logic so that counter value is always between 0 and 20
+
 function App() {
   let [counter, setCounter] = useState(0)
 
   const addValue = () => {
-    counter = counter + 1
-    setCounter(counter)
-    console.log("Clicked ", counter)
+    if (counter < 20) {
+      counter = counter + 1
+      setCounter(counter)
+      console.log("Clicked ", counter)
+    } else {
+      alert("Cannot add more than 20")
+    }
   }
 
   const removeValue = () => {
-    setCounter(counter - 1)
-    console.log("Clicked ", counter)
+    if (counter > 0) {
+      setCounter(counter - 1)
+    } else {
+      alert("Cannot go below 0")
+    }
   }
 
   return (
